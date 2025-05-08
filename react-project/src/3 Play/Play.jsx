8e0349c7615ea3283d5190631265a6b4ea0681a5
Art Monakhov <artemius135@icloud.com>
2025-05-08 22:50:26 +0300

    progress bar animated

--- react-project/src/3 Play/Play.jsx
+++ react-project/src/3 Play/Play.jsx
@@ -8,14 +8,18 @@ import { WSPlayAPI } from "../WS_communication.mjs";
 import { useLocation, useParams } from "react-router-dom";
 import "./Play.scss"
 import {QR} from "./QR.jsx";
-
 export const Play = () => {
 
     const [socket, setSocket] = useState(null)
     const [socketStatus, setSocketStatus] = useState('null')
     const [roommates, setRoommates] = useState({})
 
-    const gameStates = ['lobby', 'live', 'finished']
+    const gameStates = {
+        lobby:0, live:1, finished:2
+    }
+    const socketStates = {
+        null: -1, closed: 0, open: 1, inRoom: 2, error:3
+    }
 
     const {state} = useLocation()
     const {roomId} = useParams()
@@ -27,6 +31,7 @@ export const Play = () => {
     const [quizLength, setQuizLength] = useState(null)
     const [currentQuestionInd, setCurrentQuestionInd] = useState(null)
     const [currentQuestion, setCurrentQuestion] = useState(null)
+    const [isRoommatesHidden, setIsRoommatesHidden] = useState(true)
 
     const [results, setResults] = useState(null)
 
@@ -46,17 +51,17 @@ export const Play = () => {
           socket.emitJoin(getSelfFromLocalStorage(), roomId)
         }
       }
-      socket.eventActions.close = ()=>{setSocketStatus('closed')}
-      socket.eventActions.error = ()=>{setSocketStatus('error')}
+      socket.eventActions.close = ()=>{setSocketStatus(socketStates.closed)}
+      socket.eventActions.error = ()=>{setSocketStatus(socketStates.error)}
       socket.eventActions.create = ({roommates})=>{
         setRoommates(roommates)
-        setSocketStatus('in room')
-        setGameState('lobby')
+        setSocketStatus(socketStates.inRoom)
+        setGameState(gameStates.lobby)
       }
       socket.eventActions.join = ({roommates})=>{
         setRoommates(roommates);
-        setSocketStatus('in room')
-        setGameState('lobby')
+        setSocketStatus(socketStates.inRoom)
+        setGameState(gameStates.lobby)
       }
       socket.eventActions.joined = ({user, roommates})=>{
         console.log('alert: '+user.id+":"+user.name+" has joined");
@@ -71,11 +76,11 @@ export const Play = () => {
         setCurrentQuestion(question)
         setCurrentQuestionInd(index)
         setQuizLength(quizLength)
-        setGameState('live')
+        setGameState(gameStates.live)
       }
       socket.eventActions.end = ({results})=>{
         setResults(results)
-        setGameState('finished')
+        setGameState(gameStates.finished)
       }
 
       setSocket(socket)
@@ -84,40 +89,48 @@ export const Play = () => {
 
     return <div className="Play">
       {/* <div>socket: {socketStatus}</div> */}
-        {gameState=="lobby"? <div></div> :
+        {gameState==gameStates.lobby? <div></div> :
             <div className={"status"}>
                 <QR isSmall={true} roomId={roomId}/>
                 <div className={"id"}>{roomId}</div>
-                <progress value={currentQuestionInd / quizLength}></progress>
+                <div className={"progress-bg"}>
+                    <div className={"progress-value"} style={{width: (gameState==gameStates.finished? 1 : currentQuestionInd / quizLength)*100+"%" }} />
+                </div>
+                {/*<progress value={ gameState==gameStates.finished? 1 : currentQuestionInd / quizLength}></progress>*/}
             </div>
         }
 
-      {socketStatus == 'null'? <div/> :null}
-      {socketStatus == 'closed'? <ViewError text={'connection lost'}/> : null}
-      {socketStatus == 'open'? <ViewLoading text={'cant join room'}/> : null}
+      {socketStatus == socketStates.null? <div/> :null}
+      {socketStatus == socketStates.closed? <ViewError text={'connection lost'}/> : null}
+      {socketStatus == socketStates.open? <ViewLoading text={'cant join room'}/> : null}
 
 
-      {socketStatus == 'in room' && gameState === gameStates[0] ? <ViewLobby isHost={isHost} socket={socket} roomId={roomId} /> : null}
-      {socketStatus == 'in room' && gameState === gameStates[1] ? <ViewQuestion isHost={isHost} socket={socket} roomId={roomId} setGameState={setGameState} quizLength={quizLength} setQuizLength={setQuizLength} currentQuestionInd={currentQuestionInd} setCurrentQuestionInd={setCurrentQuestionInd} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}/> : null}
-      {socketStatus == 'in room' && gameState === gameStates[2] ? <ViewResult isHost={isHost} socket={socket} roomId={roomId} results={results} roommates={roommates} /> : null}
+      {socketStatus == socketStates.inRoom && gameState === gameStates.lobby ? <ViewLobby isHost={isHost} socket={socket} roomId={roomId} /> : null}
+      {socketStatus == socketStates.inRoom && gameState === gameStates.live ? <ViewQuestion isHost={isHost} socket={socket} roomId={roomId} setGameState={setGameState} quizLength={quizLength} setQuizLength={setQuizLength} currentQuestionInd={currentQuestionInd} setCurrentQuestionInd={setCurrentQuestionInd} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}/> : null}
+      {socketStatus == socketStates.inRoom && gameState === gameStates.finished ? <ViewResult isHost={isHost} socket={socket} roomId={roomId} results={results} roommates={roommates} /> : null}
 
-      <div className="roommates-counter"> connected players: { Object.keys(roommates).length } </div>
+      {/*<div className="roommates-counter"> connected players: { Object.keys(roommates).length } </div>*/}
       <div className="hscroll">
-        <div className="hstack roommates">
-          { Object.keys(roommates).map((userId) => <div> {roommates[userId]?.name}</div>) }
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-          <div>placeholder</div>
-        </div>
+          <div className="hstack line">
+            <button className="roommates-counter" onClick={()=>{
+                setIsRoommatesHidden(!isRoommatesHidden)
+            }}> { Object.keys(roommates).length } </button>
+            <div className={"hstack roommates "+(isRoommatesHidden?"hidden":"")}>
+              { Object.keys(roommates).map((userId) => <div>{roommates[userId]?.name}</div>) }
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+              <div>placeholder</div>
+            </div>
+          </div>
       </div>
     </div>
 }
